500df89a3037913c27eadb70dab594492fb6a58c
Jason Larson <Jason.Larson@healthsparq.com>
2018-05-15 13:35:27 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,196 @@
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/img', express.static(__dirname + '/img'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+});
+
+
+//n^2 this is gross
+function collidingWith(proj){
+  for(var id in players){
+    var player = players[id];
+    if(proj.color != player.color && player.dead == 0 && proj.x < player.x + player.width &&
+      proj.x + proj.width > player.x &&
+      proj.y < player.y + player.height &&
+      proj.height + proj.y > player.y){
+      return id;
+    }
+  }
+  return null;
+}
+
+function updateProj(){
+  for(var i = 0; i < projectiles.length; ++i){
+    var proj = projectiles[i];
+    var deltaX = 5 * Math.cos(proj.angle);
+    var deltaY = 5 * Math.sin(proj.angle);
+
+    if((deltaX >= 0 && proj.x <= 780) || (deltaX <= 0 && proj.x >= 0)) {
+      proj.x += deltaX;
+    }
+    else{
+      projectiles.splice(i, 1);
+    }
+    if((deltaY >= 0 && proj.y <= 580) || (deltaY <= 0 && proj.y >= 5)) {
+      proj.y += deltaY;
+    }
+    else{
+      projectiles.splice(i, 1);
+    }
+    var collidingId = collidingWith(proj);
+    if(collidingId){
+      projectiles.splice(i, 1);
+      players[collidingId].dead = 36;
+      players[collidingId].score -= 1;
+      players[proj.id].score += 1;
+    }
+  }
+}
+var projectiles = [];
+var players = {};
+var tanks = { //track tanks in use
+  brown: false,
+  red: false,
+  blue: false,
+  green: false,
+  pink: false
+};
+io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    var newColor;
+    if(tanks.brown == false){
+      newColor = 'brown';
+      tanks.brown = true;
+    }
+    else if(tanks.red == false){
+      newColor = 'red';
+      tanks.red = true;
+    }
+    else if(tanks.blue == false){
+      newColor = 'blue';
+      tanks.blue = true;
+    }
+    else if(tanks.green == false){
+      newColor = 'green';
+      tanks.green = true;
+    }
+    else if(tanks.pink == false){
+      newColor = 'pink';
+      tanks.pink = true;
+    }
+    else
+      newColor = 'brown';
+    players[socket.id] = {
+      x: Math.random() * 775,
+      y: Math.random() * 575,
+      width: 40,
+      height: 40,
+      angle: 0,
+      speed: 0,
+      name: (Object.keys(players).length +1),
+      color: newColor,
+      reloading: false,
+      dead: 0,
+      score: 0
+    };
+    console.log("Player " + socket.id + " connected with coordinates: " + players[socket.id].x + ", " + players[socket.id].y);
+    //console.log("and angle " + players[socket.id].angle);
+
+    socket.on('disconnect', function() {
+      console.log(socket.id + " disconnected");
+      var color = players[socket.id].color;
+      tanks[color] = false;
+      delete players[socket.id];
+    });
+  });
+
+  socket.on('controls', function(data) {
+    var player = players[socket.id] || {};
+
+    if(player.dead == 0) {
+      if (data.shoot && player.reloading <= 0) {
+        projectiles.push({
+          id: socket.id,
+          color: player.color,
+          x: player.x + 15,
+          y: player.y + 20,
+          angle: player.angle,
+          width: 5,
+          height: 5
+        });
+        player.reloading = 30;
+      }
+
+      if (data.left) {
+        if (player.angle >= .05) {
+          player.angle -= .05;
+        }
+        else
+          player.angle = 2 * Math.PI;
+      }
+      if (data.up) {
+        player.speed = 3;
+      }
+      if (data.right) {
+        if (player.angle <= 2 * Math.PI) {
+          player.angle += .05;
+        }
+        else
+          player.angle = 0;
+
+      }
+      if (data.down) {
+        player.speed = -3;
+      }
+
+      var deltaX = player.speed * Math.cos(player.angle);
+      var deltaY = player.speed * Math.sin(player.angle);
+      if ((deltaX >= 0 && player.x <= 763) || (deltaX <= 0 && player.x >= 0)) {
+        player.x += deltaX;
+      }
+      if ((deltaY >= 0 && player.y <= 565) || (deltaY <= 0 && player.y >= 5)) {
+        player.y += deltaY;
+      }
+
+    }
+    player.speed = 0;
+    player.reloading -= 1; //tick down each frame
+  });
+});
+setInterval(function() {
+  for(var id in players){
+    var player = players[id];
+    player.reloading -= 1;
+    if(player.dead > 0) {
+      player.dead -= 1;
+      if(player.dead == 0){//if the player is now alive, spawn in random spot
+        player.x = Math.random() * 775;
+        player.y = Math.random() * 575;
+      }
+    }
+  }
+  updateProj();
+  io.sockets.emit('state', players, projectiles);
+}, 1000 / 60);
---
+++ static/game.js
@@ -0,0 +1,157 @@
+var socket = io();
+socket.on('message', function(data) {
+  console.log(data);
+});
+
+function drawImageRot(img,x,y,width,height,angle){
+
+  //Convert degrees to radian
+  var rad = angle;
+
+  //Set the origin to the center of the image
+  context.translate(x + width / 2, y + height / 2);
+
+  //Rotate the canvas around the origin
+  context.rotate(rad);
+
+  //draw the image
+  context.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
+
+  //reset the canvas
+  context.rotate(rad * ( -1 ) );
+  context.translate((x + width / 2) * (-1), (y + height / 2) * (-1));
+}
+
+var controls = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+  shoot: false
+};
+
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      controls.left = true;
+      break;
+    case 87: // W
+      controls.up = true;
+      break;
+    case 68: // D
+      controls.right = true;
+      break;
+    case 83: // S
+      controls.down = true;
+      break;
+    case 32: //spacebar
+      controls.shoot = true;
+      break;
+  }
+});
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      controls.left = false;
+      break;
+    case 87: // W
+      controls.up = false;
+      break;
+    case 68: // D
+      controls.right = false;
+      break;
+    case 83: // S
+      controls.down = false;
+      break;
+    case 32: // Spacebar
+      controls.shoot = false;
+      break;
+
+  }
+});
+socket.emit('new player');
+
+setInterval(function() {
+  socket.emit('controls', controls);
+}, 1000 / 60);
+
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+socket.on('state', function(players, projectiles) {
+  context.clearRect(0, 0, 800, 600);
+  var scoreX = 8;
+  var scoreY = 10;
+  context.fillStyle='black';
+  context.fillText("Score", scoreX, scoreY);
+  for (var id in players) {
+    var player = players[id];
+    context.fillStyle = player.color;
+    scoreY += 10;
+    context.fillText(player.color + ": " + player.score, scoreX, scoreY);
+    if(player.dead == 0) { //player is not dead
+      var tank;
+      switch (player.color) {
+        case "brown":
+          tank = document.getElementById("tank1");
+          break;
+
+        case "red":
+          tank = document.getElementById("tank2");
+          break;
+        case "blue":
+          tank = document.getElementById("tank3");
+          break;
+        case "green":
+          tank = document.getElementById("tank4");
+          break;
+        case "pink":
+          tank = document.getElementById("tank5");
+          break;
+      }
+      drawImageRot(tank, player.x, player.y, 40, 40, player.angle);
+    }
+    else{//player's tank was destroyed, display explosion
+      var explosion;
+      switch(true){ //gross but works
+        case (player.dead >= 32):
+          explosion = document.getElementById('explosion9');
+          break;
+        case (player.dead < 36 *8/9 && player.dead >= 36 * 7/9):
+          explosion = document.getElementById('explosion8');
+          break;
+        case (player.dead < 36 *7/9 && player.dead >= 36 * 6/9):
+          explosion = document.getElementById('explosion7');
+          break;
+        case (player.dead < 36 *6/9 && player.dead >= 36 * 5/9):
+          explosion = document.getElementById('explosion6');
+          break;
+        case (player.dead < 36 *5/9 && player.dead >= 36 * 4/9):
+          explosion = document.getElementById('explosion5');
+          break;
+        case (player.dead < 36 *4/9 && player.dead >= 36 * 3/9):
+          explosion = document.getElementById('explosion4');
+          break;
+        case (player.dead < 36 *3/9 && player.dead >= 36 * 2/9):
+          explosion = document.getElementById('explosion3');
+          break;
+        case (player.dead < 36 *2/9 && player.dead >= 36 * 1/9):
+          explosion = document.getElementById('explosion2');
+          break;
+        case (player.dead < 36 *1/9 && player.dead > 0):
+          explosion = document.getElementById('explosion1');
+          break;
+        default:
+          explosion = explosion = document.getElementById('explosion1');
+          break;
+      }
+      drawImageRot(explosion, player.x, player.y, 40, 40, player.angle);
+    }
+  }
+  for(var i = 0; i < projectiles.length; ++i){
+    var proj = projectiles[i];
+    context.fillStyle=proj.color;
+    context.fillRect(proj.x, proj.y, 5, 5);
+  }
+});
